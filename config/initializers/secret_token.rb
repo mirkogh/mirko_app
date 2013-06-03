1caa0fd822322c73c6a50800bf1e1af32066dfde
Mirko <mirkobt@gmail.com>
2013-06-03 10:21:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MirkoApp::Application.config.secret_token = '2cd3b03464b983f5081e017b97ffa8246012653e8b0d170b215907dd4fddcbb577d825942e040715950889ee075fcf8a40fd7c1f48c7733ff9a46d4806a58757'
